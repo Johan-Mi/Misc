f2ca861214cee2bcff79881c1845addb5e042034
Johan-Mi <johanmilanov@outlook.com>
2019-11-11 21:14:27 +0100

    Medieval game commit

--- MedievalGame/main.cpp
+++ MedievalGame/main.cpp
@@ -1,20 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <unordered_map>
 
 constexpr int screenWidth = 256;
 constexpr int screenHeight = 240;
 
 struct Tile {
-	int atlasX;
-	int atlasY;
+	const sf::Vector2f atlasPos;
 };
 
-constexpr Tile tiles[] {
-	{0, 0},
+const Tile tiles[] {
+	{{0, 0}},
+	{{16, 0}},
 };
 
-int* map;
-int mapWidth;
-int mapHeight;
+struct Map {
+	unsigned width;
+	unsigned height;
+	unsigned* data;
+
+	void allocate() {
+		data = new unsigned[width * height];
+	}
+
+	unsigned& operator()(int x, int y) {
+		return data[x * width + y];
+	}
+
+	~Map() {
+		delete[] data;
+	}
+};
+
+std::unordered_map<std::string, Map> maps;
+
+Map* map;
 
 void renderMap(sf::RenderWindow&);
 void loadAllMaps();
@@ -28,6 +50,8 @@ int main() {
 	window.setView(sf::View(sf::FloatRect(0, 0, screenWidth, screenHeight)));
 
 	loadAllMaps();
+
+	map = &maps["map1"];
 
 	while (window.isOpen()) {
         sf::Event event;
@@ -56,7 +80,8 @@ void renderMap(sf::RenderWindow& window) {
 			quad[1].position = quad[0].position + sf::Vector2f(16, 0);
 			quad[2].position = quad[0].position + sf::Vector2f(16, 16);
 			quad[3].position = quad[0].position + sf::Vector2f(0, 16);
-			quad[0].texCoords = sf::Vector2f(0, 0);
+
+			quad[0].texCoords = tiles[(*map)(j, i)].atlasPos;
 			quad[1].texCoords = quad[0].texCoords + sf::Vector2f(16, 0);
 			quad[2].texCoords = quad[0].texCoords + sf::Vector2f(16, 16);
 			quad[3].texCoords = quad[0].texCoords + sf::Vector2f(0, 16);
@@ -66,5 +91,18 @@ void renderMap(sf::RenderWindow& window) {
 }
 
 void loadAllMaps() {
-	std::if mapNames();
+	std::ifstream mapNames("mapNames");
+	std::string currentName;
+	while(mapNames >> currentName) {
+		std::ifstream currentMap("Maps/" + currentName, std::ios::in | std::ios::binary);
+		std::vector<unsigned char> contents((std::istreambuf_iterator<char>(currentMap)), std::istreambuf_iterator<char>());
+		currentMap.close();
+		maps[currentName].width = *(unsigned*)&contents[0];
+		maps[currentName].height = *(unsigned*)&contents[4];
+		maps[currentName].allocate();
+		for(int i = 0; i < maps[currentName].width * maps[currentName].height; i++)
+			maps[currentName].data[i] = *(unsigned*)&contents[4 * i + 8];
+		std::clog << "Loaded map: " << currentName << "\nWidth: " << maps[currentName].width << "\nHeight: " << maps[currentName].height << '\n';
+	}
+	mapNames.close();
 }
