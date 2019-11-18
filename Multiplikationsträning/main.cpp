f00e3390f9e49b44415234e9a75b2b0b2bffaa2c
Johan-Mi <johanmilanov@outlook.com>
2019-11-18 19:39:05 +0100

    Multiplikationsträning commit

--- Multiplikationsträning/main.cpp
+++ Multiplikationsträning/main.cpp
@@ -1,34 +1,59 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <vector>
+#include <sstream>
 
 int main() {
 	std::mt19937 rng(time(0));
 
+	std::vector<int> tables;
+	std::cout << "Tabeller: ";
+	std::string line;
+	std::getline(std::cin, line);
+	std::istringstream stream(line);
+	int buffer;
+	while(stream >> buffer) {
+		tables.push_back(buffer);
+	}
+
 	int min;
 	int max;
-
 	std::cout << "Lägsta talet: ";
 	std::cin >> min;
 	std::cout << "Högsta talet: ";
 	std::cin >> max;
 
-	while(true) {
+	int questionCount;
+	std::cout << "Antal uppgifter: ";
+	std::cin >> questionCount;
+
+	int correct = 0;
+	int incorrect = 0;
+
+	while(questionCount--) {
 		std::cout << '\n';
-		int a = rng() % (max - min) + min;
-		int b = rng() % (max - min) + min;
-		std::cout << a << " * " << b << " = ";
-		std::string guess;
-		std::cin >> guess;
-		try {
-			if(std::stoi(guess) == a * b)
-				std::cout << "Rätt!\n";
-			else
-				std::cout << "Fel! Rätt svar är " << a * b << ".\n";
-		} catch(std::invalid_argument e) {
+		int a = rng() % (max - min + 1) + min;
+		int b = tables[rng() % tables.size()];
+		if(rng() % 2)
+			std::cout << a << " * " << b << " = ";
+		else
+			std::cout << b << " * " << a << " = ";
+
+		int guess;
+		if(!(std::cin >> guess))
 			return 0;
+		if(guess == a * b) {
+			std::cout << "Rätt!\n";
+			correct++;
+		}
+		else {
+			std::cout << "Fel! Rätt svar är " << a * b << ".\n";
+			incorrect++;
 		}
 	}
+
+	std::cout << "\nAntal rätt: " << correct << "\nAntal fel: " << incorrect;
 
 	return 0;
 }
