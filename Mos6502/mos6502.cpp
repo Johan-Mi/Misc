65eaf11e568045b7b0d7fcd42d7332047a2363a6
Johan-Mi <johanmilanov@outlook.com>
2019-11-26 18:07:46 +0100

    Mos6502 commit

--- Mos6502/mos6502.cpp
+++ Mos6502/mos6502.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <cassert>
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -33,6 +34,11 @@ class Mos6502 {
 		flags.Z = !dest;
 		flags.N = dest >> 7;
 	}
+	bool conditionalBranch(bool condition, i8 value) {
+		if(condition)
+			PC += value;
+		return condition;
+	}
 
 	void ADC(const u8 value) {
 		// TODO: Add support for decimal mode
@@ -54,55 +60,23 @@ class Mos6502 {
 		flags.Z = !value;
 		flags.N = value >> 7;
 	}
-	bool BCC(const i8 value) {
-		if(!flags.C)
-			PC += value;
-		return !flags.C;
-	}
-	bool BCS(const i8 value) {
-		if(flags.C)
-			PC += value;
-		return flags.C;
-	}
-	bool BEQ(const i8 value) {
-		if(flags.Z)
-			PC += value;
-		return flags.Z;
-	}
+	bool BCC(const i8 value) { return conditionalBranch(!flags.C, value); }
+	bool BCS(const i8 value) { return conditionalBranch( flags.C, value); }
+	bool BEQ(const i8 value) { return conditionalBranch( flags.Z, value); }
 	void BIT(const u8 value) {
 		u8 result = A & value;
 		flags.Z = !result;
 		flags.N = result >> 7;
 		flags.V = result >> 6 & 1;
 	}
-	bool BMI(const i8 value) {
-		if(flags.N)
-			PC += value;
-		return flags.N;
-	}
-	bool BNE(const i8 value) {
-		if(!flags.Z)
-			PC += value;
-		return !flags.Z;
-	}
-	bool BPL(const i8 value) {
-		if(!flags.N)
-			PC += value;
-		return !flags.N;
-	}
+	bool BMI(const i8 value) { return conditionalBranch( flags.N, value); }
+	bool BNE(const i8 value) { return conditionalBranch(!flags.Z, value); }
+	bool BPL(const i8 value) { return conditionalBranch(!flags.N, value); }
 	void BRK() {
 		// TODO: Implement interrupts
 	}
-	bool BVC(const i8 value) {
-		if(!flags.V)
-			PC += value;
-		return !flags.V;
-	}
-	bool BVS(const i8 value) {
-		if(flags.V)
-			PC += value;
-		return flags.V;
-	}
+	bool BVC(const i8 value) { return conditionalBranch(!flags.V, value); }
+	bool BVS(const i8 value) { return conditionalBranch( flags.V, value); }
 	void CLC() { flags.C = 0; }
 	void CLD() { flags.D = 0; }
 	void CLI() { flags.I = 0; }
@@ -198,6 +172,8 @@ class Mos6502 {
 	void TXS() { movGeneric(X, S); }
 	void TYA() { movGeneric(Y, A); }
 
+	void invalidOpcode() { assert(!"Invalid opcode"); }
+
 
 
 public:
@@ -213,352 +189,373 @@ public:
 	virtual u8 readByte(u16 addresss);
 	virtual void writeByte(u16 address, u8 value);
 
-	u16 readWord(u16 address) {
-		return (u16)readByte(address + 1) << 8 | readByte(address);
-	}
+	u16 readWord(u16 address) { return (u16)readByte(address + 1) << 8 | readByte(address); }
 	u16 writeWord(u16 address, u16 value) {
 		writeByte(address, value);
 		writeByte(address + 1, value >> 8);
 	}
 
-	u8 popByte() {
-		return readByte(++S);
-	}
-	void pushByte(u8 value) {
-		writeByte(S--, value);
-	}
-
-	u16 popWord() {
-		return popByte() | (u16)popByte() >> 8;
-	}
+	u8 popByte() { return readByte(++S); }
+	void pushByte(u8 value) { writeByte(S--, value); }
+	u16 popWord() { return popByte() | (u16)popByte() >> 8; }
 	void pushWord(u16 value) {
 		pushByte(value >> 8);
 		pushByte(value);
 	}
 
+	const u8 operandImm() { return readByte(PC + 1); }
+	const u8 operandImmWord() { return readWord(PC + 1); }
+	const u8 operandZpg() { return readByte(readByte(PC + 1)); }
+	const u8 operandAbs() { return readByte(readWord(PC + 1)); }
+
 	void step() {
-		switch(readByte(PC++)) {
+		switch(readByte(PC)) {
 		case 0x00: // BRK
-			BRK();
+			//BRK();
+			// TODO: Implement interrupts
+			assert(!"BRK hasn't been implemented yet");
 			break;
 		case 0x01:
-		case 0x02:
-		case 0x03:
-		case 0x04:
-		case 0x05:
+		case 0x05: // ORA zpg
+			ORA(operandZpg());
+			PC += 2;
+			break;
 		case 0x06:
-		case 0x07:
 		case 0x08: // PHP
 			PHP();
+			PC++;
 			break;
-		case 0x09:
+		case 0x09: // ORA #
+			ORA(operandImm());
+			PC += 2;
+			break;
 		case 0x0a:
-		case 0x0b:
-		case 0x0c:
 		case 0x0d:
 		case 0x0e:
-		case 0x0f: // Invalid
+		case 0x10: // BPL rel
+			BPL(operandImm());
+			PC += 2;
 			break;
-		case 0x10:
 		case 0x11:
-		case 0x12:
-		case 0x13:
-		case 0x14:
 		case 0x15:
 		case 0x16:
-		case 0x17:
 		case 0x18: // CLC
 			CLC();
+			PC++;
 			break;
 		case 0x19:
-		case 0x1a:
-		case 0x1b:
-		case 0x1c:
 		case 0x1d:
 		case 0x1e:
-		case 0x1f: // Invalid
+		case 0x20: // JSR abs
+			JSR(operandImmWord());
 			break;
-		case 0x20:
 		case 0x21:
-		case 0x22:
-		case 0x23:
-		case 0x24:
-		case 0x25:
+		case 0x24: // BIT zpg
+			BIT(operandZpg());
+			PC += 2;
+			break;
+		case 0x25: // AND zpg
+			AND(operandZpg());
+			PC += 2;
+			break;
 		case 0x26:
-		case 0x27:
 		case 0x28: // PLP
 			PLP();
+			PC++;
 			break;
-		case 0x29:
+		case 0x29: // AND #
+			AND(operandImm());
+			PC += 2;
+			break;
 		case 0x2a:
-		case 0x2b:
-		case 0x2c:
+		case 0x2c: // BIT abs
+			BIT(operandAbs());
+			PC += 3;
+			break;
 		case 0x2d:
 		case 0x2e:
-		case 0x2f: // Invalid
+		case 0x30: // BMI rel
+			BMI(operandImm());
+			PC += 2;
 			break;
-		case 0x30:
 		case 0x31:
-		case 0x32:
-		case 0x33:
-		case 0x34:
 		case 0x35:
 		case 0x36:
-		case 0x37:
 		case 0x38: // SEC
 			SEC();
+			PC++;
 			break;
 		case 0x39:
-		case 0x3a:
-		case 0x3b:
 		case 0x3c:
 		case 0x3d:
 		case 0x3e:
-		case 0x3f: // Invalid
-			break;
 		case 0x40: // RTI
 			RTI();
+			PC++;
 			break;
 		case 0x41:
-		case 0x42:
-		case 0x43:
-		case 0x44:
-		case 0x45:
+		case 0x45: // EOR zpg
+			EOR(operandZpg());
+			PC += 2;
+			break;
 		case 0x46:
-		case 0x47:
 		case 0x48: // PHA
 			PHA();
+			PC++;
 			break;
-		case 0x49:
+		case 0x49: // EOR #
+			EOR(operandImm());
+			PC += 2;
+			break;
 		case 0x4a:
-		case 0x4b:
-		case 0x4c:
+		case 0x4c: // JMP abs
+			JMP(operandImmWord());
+			PC += 3;
+			break;
 		case 0x4d:
 		case 0x4e:
-		case 0x4f: // Invalid
+		case 0x50: // BVC rel
+			BVC(operandImm());
+			PC += 2;
 			break;
-		case 0x50:
 		case 0x51:
-		case 0x52:
-		case 0x53:
-		case 0x54:
 		case 0x55:
 		case 0x56:
-		case 0x57:
 		case 0x58: // CLI
 			CLI();
+			PC++;
 			break;
 		case 0x59:
-		case 0x5a:
-		case 0x5b:
 		case 0x5c:
 		case 0x5d:
 		case 0x5e:
-		case 0x5f: // Invalid
-			break;
 		case 0x60: // RTS
 			RTS();
+			PC++;
 			break;
 		case 0x61:
-		case 0x62:
-		case 0x63:
-		case 0x64:
-		case 0x65:
+		case 0x65: // ADC zpg
+			ADC(operandZpg());
+			PC += 2;
+			break;
 		case 0x66:
-		case 0x67:
 		case 0x68: // PLA
 			PLA();
+			PC++;
 			break;
-		case 0x69:
+		case 0x69: // ADC #
+			ADC(operandImm());
+			PC += 2;
+			break;
 		case 0x6a:
-		case 0x6b:
 		case 0x6c:
 		case 0x6d:
 		case 0x6e:
-		case 0x6f: // Invalid
+		case 0x70: // BVS rel
+			BVS(operandImm());
+			PC += 2;
 			break;
-		case 0x70:
 		case 0x71:
-		case 0x72:
-		case 0x73:
-		case 0x74:
 		case 0x75:
 		case 0x76:
-		case 0x77:
 		case 0x78: // SEI
 			SEI();
+			PC++;
 			break;
 		case 0x79:
-		case 0x7a:
-		case 0x7b:
 		case 0x7c:
 		case 0x7d:
 		case 0x7e:
-		case 0x7f: // Invalid
-			break;
-		case 0x80:
 		case 0x81:
-		case 0x82:
-		case 0x83:
 		case 0x84:
 		case 0x85:
 		case 0x86:
-		case 0x87:
 		case 0x88: // DEY
 			DEY();
+			PC++;
 			break;
-		case 0x89:
 		case 0x8a: // TXA
 			TXA();
+			PC++;
 			break;
-		case 0x8b:
 		case 0x8c:
 		case 0x8d:
 		case 0x8e:
-		case 0x8f: // Invalid
+		case 0x90: // BCC rel
+			BCC(operandImm());
+			PC += 2;
 			break;
-		case 0x90:
 		case 0x91:
-		case 0x92:
-		case 0x93:
 		case 0x94:
 		case 0x95:
 		case 0x96:
-		case 0x97:
 		case 0x98: // TYA
 			TYA();
+			PC++;
 			break;
 		case 0x99:
 		case 0x9a: // TXS
 			TXS();
+			PC++;
 			break;
-		case 0x9b:
 		case 0x9c:
 		case 0x9d:
-		case 0x9e:
-		case 0x9f: // Invalid
+		case 0xa0: // LDY #
+			LDY(operandImm());
+			PC += 2;
 			break;
-		case 0xa0:
 		case 0xa1:
-		case 0xa2:
-		case 0xa3:
-		case 0xa4:
-		case 0xa5:
-		case 0xa6:
-		case 0xa7:
+		case 0xa2: // LDX #
+			LDX(operandImm());
+			PC += 2;
+			break;
+		case 0xa4: // LDY zpg
+			LDY(operandZpg());
+			PC += 2;
+			break;
+		case 0xa5: // LDA zpg
+			LDA(operandZpg());
+			PC += 2;
+			break;
+		case 0xa6: // LDX zpg
+			LDX(operandZpg());
+			PC += 2;
+			break;
 		case 0xa8: // TAY
 			TAY();
+			PC++;
 			break;
-		case 0xa9:
+		case 0xa9: // LDA #
+			LDA(operandImm());
+			PC += 2;
+			break;
 		case 0xaa: // TAX
 			TAX();
+			PC++;
 			break;
-		case 0xab:
-		case 0xac:
+		case 0xac: // LDY abs
+			LDY(operandAbs());
+			PC += 3;
+			break;
 		case 0xad:
 		case 0xae:
-		case 0xaf: // Invalid
+		case 0xb0: // BCS rel
+			BCS(operandImm());
+			PC += 2;
 			break;
-		case 0xb0:
 		case 0xb1:
-		case 0xb2:
-		case 0xb3:
 		case 0xb4:
 		case 0xb5:
 		case 0xb6:
-		case 0xb7:
 		case 0xb8: // CLV
 			CLV();
+			PC++;
 			break;
 		case 0xb9:
 		case 0xba: // TSX
 			TSX();
+			PC++;
 			break;
-		case 0xbb:
 		case 0xbc:
 		case 0xbd:
 		case 0xbe:
-		case 0xbf: // Invalid
+		case 0xc0: // CPY #
+			CPY(operandImm());
+			PC += 2;
 			break;
-		case 0xc0:
 		case 0xc1:
-		case 0xc2:
-		case 0xc3:
-		case 0xc4:
-		case 0xc5:
+		case 0xc4: // CPY zpg
+			CPY(operandZpg());
+			PC += 2;
+			break;
+		case 0xc5: // CMP zpg
+			CMP(operandZpg());
+			PC += 2;
+			break;
 		case 0xc6:
-		case 0xc7:
 		case 0xc8: // INY
 			INY();
+			PC++;
 			break;
-		case 0xc9:
+		case 0xc9: // CMP #
+			CMP(operandImm());
+			PC += 2;
+			break;
 		case 0xca: // DEX
 			DEX();
+			PC++;
 			break;
-		case 0xcb:
-		case 0xcc:
+		case 0xcc: // CPY abs
+			CPY(operandAbs());
+			PC += 3;
+			break;
 		case 0xcd:
 		case 0xce:
-		case 0xcf: // Invalid
+		case 0xd0: // BNE rel
+			BNE(operandImm());
+			PC += 2;
 			break;
-		case 0xd0:
 		case 0xd1:
-		case 0xd2:
-		case 0xd3:
-		case 0xd4:
 		case 0xd5:
 		case 0xd6:
-		case 0xd7:
 		case 0xd8: // CLD
 			CLD();
+			PC++;
 			break;
 		case 0xd9:
-		case 0xda:
-		case 0xdb:
 		case 0xdc:
 		case 0xdd:
 		case 0xde:
-		case 0xdf: // Invalid
+		case 0xe0: // CPX #
+			CPX(operandImm());
+			PC += 2;
 			break;
-		case 0xe0:
 		case 0xe1:
-		case 0xe2:
-		case 0xe3:
-		case 0xe4:
-		case 0xe5:
+		case 0xe4: // CPX zpg
+			CPX(operandZpg());
+			PC += 2;
+			break;
+		case 0xe5: // SBC zpg
+			SBC(operandZpg());
+			PC += 2;
+			break;
 		case 0xe6:
-		case 0xe7:
 		case 0xe8: // INX
 			INX();
+			PC++;
 			break;
-		case 0xe9:
+		case 0xe9: // SBC #
+			SBC(operandImm());
+			PC += 2;
+			break;
 		case 0xea: // NOP
 			NOP();
+			PC++;
 			break;
-		case 0xeb:
-		case 0xec:
+		case 0xec: // CPX abs
+			CPX(operandAbs());
+			PC += 3;
+			break;
 		case 0xed:
 		case 0xee:
-		case 0xef: // Invalid
+		case 0xf0: // BEQ rel
+			BEQ(operandImm());
+			PC += 2;
 			break;
-		case 0xf0:
 		case 0xf1:
-		case 0xf2:
-		case 0xf3:
-		case 0xf4:
 		case 0xf5:
 		case 0xf6:
-		case 0xf7:
 		case 0xf8: // SED
 			SED();
+			PC++;
 			break;
 		case 0xf9:
-		case 0xfa:
-		case 0xfb:
 		case 0xfc:
 		case 0xfd:
 		case 0xfe:
-		case 0xff: // Invalid
+
+		default: // Invalid
+			invalidOpcode();
 			break;
 		}
 	}
