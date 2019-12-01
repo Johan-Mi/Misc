7fcbeae6cd789a223769464a283ea2239cef46ad
Johan-Mi <johanmilanov@outlook.com>
2019-12-01 14:24:32 +0100

    Mos6502 commit

--- Mos6502/mos6502.cpp
+++ Mos6502/mos6502.cpp
@@ -29,9 +29,8 @@ class Mos6502 {
 
 	struct MemLocation {
 		u16 address;
-		operator u16&() {
-			return address;
-		}
+		MemLocation(u16 a) : address(a) {}
+		operator u16&() { return address; }
 	};
 
 	u8 read(u8 location) { return location; }
@@ -86,7 +85,10 @@ class Mos6502 {
 	bool BNE(const i8 value) { return conditionalBranch(!flags.Z, value); }
 	bool BPL(const i8 value) { return conditionalBranch(!flags.N, value); }
 	void BRK() {
-		// TODO: Implement interrupts
+		bool oldB = flags.B;
+		flags.B = true;
+		sendIrq();
+		flags.B = oldB;
 	}
 	bool BVC(const i8 value) { return conditionalBranch(!flags.V, value); }
 	bool BVS(const i8 value) { return conditionalBranch( flags.V, value); }
@@ -199,13 +201,19 @@ class Mos6502 {
 
 
 public:
-	void reset() {
+	void sendReset() {
 		flags.B = 1;
 		flags.D = 0;
 		flags.I = 1;
 		flags._ = 1;
 		PC = readWord(0xfffc);
 		S = 0xff;
+	}
+	void sendNmi() {
+
+	}
+	void sendIrq() {
+
 	}
 
 	virtual u8 readByte(u16 addresss);
@@ -225,17 +233,16 @@ public:
 		pushByte(value);
 	}
 
-	MemLocation operandImm()		{ return {(u16)(PC + 1)}; }
-	MemLocation operandImmWord()	{ return {(u16)(PC + 1)}; }
-	MemLocation operandZpg()		{ return {readByte(PC + 1)}; }
-	MemLocation operandAbs()		{ return {readWord(PC + 1)}; }
-	MemLocation operandAbsX()		{ return {readWord(PC + 1) + X}; }
-	MemLocation operandAbsY()		{ return {readWord(PC + 1) + Y}; }
-	MemLocation operandXInd()		{ return {readWord((readByte(PC + 1) + X) & 0xff)}; }
-	MemLocation operandIndY()		{ return {readWord(readByte(PC + 1)) + Y}; }
-	MemLocation operandInd()		{ return {readWord(readWord(PC + 1))}; }
-	MemLocation operandZpgX()		{ return { (readByte(PC + 1) + X) & 0xff}; }
-	MemLocation operandZpgY()		{ return { (readByte(PC + 1) + Y) & 0xff}; }
+	MemLocation operandImm()	{ return PC + 1; }
+	MemLocation operandZpg()	{ return readByte(PC + 1); }
+	MemLocation operandAbs()	{ return readWord(PC + 1); }
+	MemLocation operandAbsX()	{ return readWord(PC + 1) + X; }
+	MemLocation operandAbsY()	{ return readWord(PC + 1) + Y; }
+	MemLocation operandXInd()	{ return readWord((readByte(PC + 1) + X) & 0xff); }
+	MemLocation operandIndY()	{ return readWord(readByte(PC + 1)) + Y; }
+	MemLocation operandInd()	{ return readWord(readWord(PC + 1)); }
+	MemLocation operandZpgX()	{ return (readByte(PC + 1) + X) & 0xff; }
+	MemLocation operandZpgY()	{ return (readByte(PC + 1) + Y) & 0xff; }
 
 	void step() {
 		switch(readByte(PC)) {
@@ -309,7 +316,7 @@ public:
 			PC += 3;
 			break;
 		case 0x20: // JSR abs
-			JSR(operandImmWord());
+			JSR(operandAbs());
 			break;
 		case 0x21: // AND X,ind
 			AND(operandXInd());
@@ -355,27 +362,50 @@ public:
 			BMI(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0x31:
-		case 0x35:
-		case 0x36:
+		case 0x31: // AND ind,Y
+			AND(operandIndY());
+			PC += 2;
+			break;
+		case 0x35: // AND zpg,X
+			AND(operandZpgX());
+			PC += 2;
+			break;
+		case 0x36: // ROL zpg,X
+			ROL(operandZpgX());
+			PC += 2;
+			break;
 		case 0x38: // SEC
 			SEC();
 			PC++;
 			break;
-		case 0x39:
-		case 0x3c:
-		case 0x3d:
-		case 0x3e:
+		case 0x39: // AND abs,Y
+			AND(operandAbsY());
+			PC += 3;
+			break;
+		case 0x3d: // AND abs,X
+			AND(operandAbsX());
+			PC += 3;
+			break;
+		case 0x3e: // LSR abs
+			LSR(operandAbs());
+			PC += 3;
+			break;
 		case 0x40: // RTI
 			RTI();
 			PC++;
 			break;
-		case 0x41:
+		case 0x41: // EOR X,ind
+			EOR(operandXInd());
+			PC += 2;
+			break;
 		case 0x45: // EOR zpg
 			EOR(operandZpg());
 			PC += 2;
 			break;
-		case 0x46:
+		case 0x46: // LSR zpg
+			LSR(operandZpg());
+			PC += 2;
+			break;
 		case 0x48: // PHA
 			PHA();
 			PC++;
@@ -384,38 +414,70 @@ public:
 			EOR(operandImm());
 			PC += 2;
 			break;
-		case 0x4a:
+		case 0x4a: // LSR A
+			LSR(A);
+			PC++;
+			break;
 		case 0x4c: // JMP abs
-			JMP(operandImmWord());
+			JMP(operandAbs());
 			PC += 3;
 			break;
-		case 0x4d:
-		case 0x4e:
+		case 0x4d: // EOR abs
+			EOR(operandAbs());
+			PC += 3;
+			break;
+		case 0x4e: // LSR abs
+			LSR(operandAbs());
+			PC += 3;
+			break;
 		case 0x50: // BVC rel
 			BVC(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0x51:
-		case 0x55:
-		case 0x56:
+		case 0x51: // EOR ind,Y
+			EOR(operandIndY());
+			PC += 2;
+			break;
+		case 0x55: // EOR zpg,X
+			EOR(operandZpgX());
+			PC += 2;
+			break;
+		case 0x56: // LSR zpg,X
+			LSR(operandZpgX());
+			PC += 2;
+			break;
 		case 0x58: // CLI
 			CLI();
 			PC++;
 			break;
-		case 0x59:
-		case 0x5c:
-		case 0x5d:
-		case 0x5e:
+		case 0x59: // EOR abs,Y
+			EOR(operandAbsY());
+			PC += 3;
+			break;
+		case 0x5d: // EOR abs,X
+			EOR(operandAbsX());
+			PC += 3;
+			break;
+		case 0x5e: // LSR abs,X
+			LSR(operandAbsX());
+			PC += 3;
+			break;
 		case 0x60: // RTS
 			RTS();
 			PC++;
 			break;
-		case 0x61:
+		case 0x61: // ADC X,ind
+			ADC(operandXInd());
+			PC += 2;
+			break;
 		case 0x65: // ADC zpg
 			ADC(operandZpg());
 			PC += 2;
 			break;
-		case 0x66:
+		case 0x66: // ROR zpg
+			ROR(operandZpg());
+			PC += 2;
+			break;
 		case 0x68: // PLA
 			PLA();
 			PC++;
@@ -424,29 +486,70 @@ public:
 			ADC(operandImm());
 			PC += 2;
 			break;
-		case 0x6a:
-		case 0x6c:
-		case 0x6d:
-		case 0x6e:
+		case 0x6a: // ROR A
+			ROR(A);
+			PC++;
+			break;
+		case 0x6c: // JMP ind
+			JMP(operandInd());
+			PC += 3;
+			break;
+		case 0x6d: // ADC abs
+			ADC(operandAbs());
+			PC += 3;
+			break;
+		case 0x6e: // ROR abs
+			ROR(operandAbs());
+			PC += 3;
+			break;
 		case 0x70: // BVS rel
 			BVS(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0x71:
-		case 0x75:
-		case 0x76:
+		case 0x71: // ADC ind,Y
+			ADC(operandIndY());
+			PC += 2;
+			break;
+		case 0x75: // ADC zpg,X
+			ADC(operandZpgX());
+			PC += 2;
+			break;
+		case 0x76: // ROR zpg,X
+			ROR(operandZpgX());
+			PC += 2;
+			break;
 		case 0x78: // SEI
 			SEI();
 			PC++;
 			break;
-		case 0x79:
-		case 0x7c:
-		case 0x7d:
-		case 0x7e:
-		case 0x81:
-		case 0x84:
-		case 0x85:
-		case 0x86:
+		case 0x79: // ADC abs,Y
+			ADC(operandAbsY());
+			PC += 3;
+			break;
+		case 0x7d: // ADC abs,X
+			ADC(operandAbsX());
+			PC += 3;
+			break;
+		case 0x7e: // ROR abs,X
+			ROR(operandAbsX());
+			PC += 3;
+			break;
+		case 0x81: // STA X,ind
+			STA(operandXInd());
+			PC += 2;
+			break;
+		case 0x84: // STY zpg
+			STY(operandZpg());
+			PC += 2;
+			break;
+		case 0x85: // STA zpg
+			STA(operandZpg());
+			PC += 2;
+			break;
+		case 0x86: // STX zpg
+			STX(operandZpg());
+			PC += 2;
+			break;
 		case 0x88: // DEY
 			DEY();
 			PC++;
@@ -455,33 +558,62 @@ public:
 			TXA();
 			PC++;
 			break;
-		case 0x8c:
-		case 0x8d:
-		case 0x8e:
+		case 0x8c: // STY abs
+			STY(operandAbs());
+			PC += 3;
+			break;
+		case 0x8d: // STA abs
+			STA(operandAbs());
+			PC += 3;
+			break;
+		case 0x8e: // STX abs
+			STX(operandAbs());
+			PC += 3;
+			break;
 		case 0x90: // BCC rel
 			BCC(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0x91:
-		case 0x94:
-		case 0x95:
-		case 0x96:
+		case 0x91: // STA ind,Y
+			STA(operandIndY());
+			PC += 2;
+			break;
+		case 0x94: // STY zpg,X
+			STY(operandZpgX());
+			PC += 2;
+			break;
+		case 0x95: // STA zpg,X
+			STA(operandZpgX());
+			PC += 2;
+			break;
+		case 0x96: // STA zpg,Y
+			STA(operandZpgY());
+			PC += 2;
+			break;
 		case 0x98: // TYA
 			TYA();
 			PC++;
 			break;
-		case 0x99:
+		case 0x99: // STA abs,Y
+			STA(operandAbsY());
+			PC += 3;
+			break;
 		case 0x9a: // TXS
 			TXS();
 			PC++;
 			break;
-		case 0x9c:
-		case 0x9d:
+		case 0x9d: // STA abs,X
+			STA(operandAbsX());
+			PC += 3;
+			break;
 		case 0xa0: // LDY #
 			LDY(operandImm());
 			PC += 2;
 			break;
-		case 0xa1:
+		case 0xa1: // LDA X,ind
+			LDA(operandXInd());
+			PC += 2;
+			break;
 		case 0xa2: // LDX #
 			LDX(operandImm());
 			PC += 2;
@@ -514,33 +646,66 @@ public:
 			LDY(operandAbs());
 			PC += 3;
 			break;
-		case 0xad:
-		case 0xae:
+		case 0xad: // LDA abs
+			LDA(operandAbs());
+			PC += 3;
+			break;
+		case 0xae: // LDX abs
+			LDX(operandAbs());
+			PC += 3;
+			break;
 		case 0xb0: // BCS rel
 			BCS(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0xb1:
-		case 0xb4:
-		case 0xb5:
-		case 0xb6:
+		case 0xb1: // LDA ind,Y
+			LDA(operandIndY());
+			PC += 2;
+			break;
+		case 0xb4: // LDY zpg,X
+			LDY(operandZpgX());
+			PC += 2;
+			break;
+		case 0xb5: // LDA zpg,X
+			LDA(operandZpgX());
+			PC += 2;
+			break;
+		case 0xb6: // LDX zpg,Y
+			LDX(operandZpgY());
+			PC += 2;
+			break;
 		case 0xb8: // CLV
 			CLV();
 			PC++;
 			break;
-		case 0xb9:
+		case 0xb9: // LDA abs,Y
+			LDA(operandAbsY());
+			PC += 3;
+			break;
 		case 0xba: // TSX
 			TSX();
 			PC++;
 			break;
-		case 0xbc:
-		case 0xbd:
-		case 0xbe:
+		case 0xbc: // LDY abs,X
+			LDY(operandAbsX());
+			PC += 3;
+			break;
+		case 0xbd: // LDA abs,X
+			LDA(operandAbsX());
+			PC += 3;
+			break;
+		case 0xbe: // LDX abs,Y
+			LDX(operandAbsY());
+			PC += 3;
+			break;
 		case 0xc0: // CPY #
 			CPY(operandImm());
 			PC += 2;
 			break;
-		case 0xc1:
+		case 0xc1: // CMP X,ind
+			CMP(operandXInd());
+			PC += 2;
+			break;
 		case 0xc4: // CPY zpg
 			CPY(operandZpg());
 			PC += 2;
@@ -549,7 +714,10 @@ public:
 			CMP(operandZpg());
 			PC += 2;
 			break;
-		case 0xc6:
+		case 0xc6: // DEC zpg
+			DEC(operandZpg());
+			PC += 2;
+			break;
 		case 0xc8: // INY
 			INY();
 			PC++;
@@ -566,28 +734,54 @@ public:
 			CPY(operandAbs());
 			PC += 3;
 			break;
-		case 0xcd:
-		case 0xce:
+		case 0xcd: // CMP abs
+			CMP(operandAbs());
+			PC += 3;
+			break;
+		case 0xce: // DEC abs
+			DEC(operandAbs());
+			PC += 3;
+			break;
 		case 0xd0: // BNE rel
 			BNE(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0xd1:
-		case 0xd5:
-		case 0xd6:
+		case 0xd1: // CMP ind,Y
+			CMP(operandIndY());
+			PC += 2;
+			break;
+		case 0xd5: // CMP zpg,X
+			CMP(operandZpgX());
+			PC += 2;
+			break;
+		case 0xd6: // DEC zpg,X
+			DEC(operandZpgX());
+			PC += 2;
+			break;
 		case 0xd8: // CLD
 			CLD();
 			PC++;
 			break;
-		case 0xd9:
-		case 0xdc:
-		case 0xdd:
-		case 0xde:
+		case 0xd9: // CMP abs,Y
+			CMP(operandAbsY());
+			PC += 3;
+			break;
+		case 0xdd: // CMP abs,X
+			CMP(operandAbsX());
+			PC += 3;
+			break;
+		case 0xde: // DEC abs,X
+			DEC(operandAbsX());
+			PC += 3;
+			break;
 		case 0xe0: // CPX #
 			CPX(operandImm());
 			PC += 2;
 			break;
-		case 0xe1:
+		case 0xe1: // SBC X,ind
+			SBC(operandXInd());
+			PC += 2;
+			break;
 		case 0xe4: // CPX zpg
 			CPX(operandZpg());
 			PC += 2;
@@ -596,7 +790,10 @@ public:
 			SBC(operandZpg());
 			PC += 2;
 			break;
-		case 0xe6:
+		case 0xe6: // INC zpg
+			INC(operandZpg());
+			PC += 2;
+			break;
 		case 0xe8: // INX
 			INX();
 			PC++;
@@ -613,15 +810,30 @@ public:
 			CPX(operandAbs());
 			PC += 3;
 			break;
-		case 0xed:
-		case 0xee:
+		case 0xed: // SBC abs
+			SBC(operandAbs());
+			PC += 3;
+			break;
+		case 0xee: // INC abs
+			INC(operandAbs());
+			PC += 3;
+			break;
 		case 0xf0: // BEQ rel
 			BEQ(readByte(PC + 1));
 			PC += 2;
 			break;
-		case 0xf1:
-		case 0xf5:
-		case 0xf6:
+		case 0xf1: // SBC ind,Y
+			SBC(operandIndY());
+			PC += 2;
+			break;
+		case 0xf5: // SBC zpg,X
+			SBC(operandZpgX());
+			PC += 2;
+			break;
+		case 0xf6: // INC zpg,X
+			INC(operandZpgX());
+			PC += 2;
+			break;
 		case 0xf8: // SED
 			SED();
 			PC++;
@@ -646,7 +858,7 @@ public:
 	}
 
 	Mos6502() {
-		reset();
+		sendReset();
 	}
 	
 };
