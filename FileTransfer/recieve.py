4d834ef7afe49f3c664a343c5c15f0aeff9bd18d
Johan-Mi <johanmilanov@outlook.com>
2020-01-17 17:15:52 +0100

    -

--- FileTransfer/recieve.py
+++ FileTransfer/recieve.py
@@ -14,11 +14,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
-        size = conn.recv(1024).decode('utf8')
-        size = size.rstrip('\0')
-        size = int(size)
-        content = conn.recv(size)
+        with open('recieved.txt', 'wb') as f:
+            data = conn.recv(1024)
+            while data:
+                f.write(data)
+                data = conn.recv(1024)
 
-with open('recieved.txt', 'wb') as f:
-    f.write(content)
 print('Recieved file!')
--- FileTransfer/send.py
+++ FileTransfer/send.py
@@ -7,16 +7,15 @@ host = input('IP address of reciever (Blank for localhost): ')
 if not host:
     host = '127.0.0.1'
 port = input('Port of reciever (Blank for 65432): ')
-if not port:
+if port:
+    port = int(port)
+else:
     port = 65432
-
-with open(filename, 'rb') as f:
-    content = f.read()
-size = len(content)
-encodedSize = str(size).encode('utf8')
-encodedSize = b''.join([encodedSize, bytes(1024 - len(encodedSize))])
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
-    s.sendall(encodedSize)
-    s.sendall(content)
+    with open(filename, 'rb') as f:
+        data = f.read(1024)
+        while data:
+            s.send(data)
+            data = f.read(1024)
