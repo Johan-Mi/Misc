c6a93694363426f1c96528ad7512e72142d5723f
Johan-Mi <johanmilanov@outlook.com>
2020-04-11 12:31:55 +0200

    -

--- 3D/main.c
+++ 3D/main.c
@@ -3,49 +3,25 @@
 #include <stdio.h>
 #include <math.h>
 #include <SDL2/SDL.h>
+#include <GL/gl.h>
+
+#include "types.h"
 
 #define LENGTH(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480 
 
-#define POINT_SIZE 5
+#define MOVE_SPEED 5.0f
+#define TURN_SPEED 3.0f
+#define ZSCALE 1
 
-#define MOVE_SPEED 0.005f
-#define TURN_SPEED 0.003f
-#define ZSCALE 500.0f
+Float3d viewTransform(Float3d);
+bool zClipLine(Float3d*, Float3d*);
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+Camera cam;
 
-typedef struct {
-	float x, y, z;
-} float3d;
-
-struct {
-	float x, y, z;
-	float rx, ry;
-} cam;
-
-float3d viewTransform(float3d p) {
-	float x = p.x - cam.x;
-	float y = p.y - cam.y;
-	float z = p.z - cam.z;
-
-	float const z2 = x * cosf(cam.ry) + z * sinf(cam.ry);
-	x = z * cosf(cam.ry) - x * sinf(cam.ry);
-	z = z2 * cosf(cam.rx) - y * sinf(cam.rx);
-	y = z2 * sinf(cam.rx) + y * cosf(cam.rx);
-
-	if(z == 0.0f)
-		z = 0.0000001f;
-	x *= ZSCALE / z;
-	y *= ZSCALE / z;
-
-	return (float3d){ x, y, z };
-}
-
-float3d const points[] = {
+Float3d const points[] = {
 	{ 0, 0, 0 },
 	{ 0, 0, 1 },
 	{ 0, 1, 0 },
@@ -71,7 +47,6 @@ size_t const lines[][2] = {
 	{ 6, 7 },
 };
 
-
 int main(void) {
 	bool quit = false;
 
@@ -81,31 +56,54 @@ int main(void) {
 
 	Uint8 const *keys = SDL_GetKeyboardState(NULL);
 
+	SDL_Window *window = NULL;
+	SDL_GLContext glcontext;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("3D",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_OPENGL);
+	glcontext = SDL_GL_CreateContext(window);
+	
 	cam.x = 0.5f;
 	cam.y = 0.5f;
 	cam.z = -3.0f;
 	cam.rx = 0.0f;
 	cam.ry = M_PI_2;
 
-	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("3D",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			SCREEN_WIDTH,
-			SCREEN_HEIGHT,
-			SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_GL_SetSwapInterval(1);
+
+	glPointSize(5);
+	glClearColor(0xff, 0xff, 0xff, 0xff);
+	glColor3b(0x00, 0x00, 0x00);
 
 	while(!quit) {
 		SDL_Event event;
 
 		prevTime = currTime;
 		currTime = SDL_GetPerformanceCounter();
-		deltaTime = (float)((currTime - prevTime) * 1000.0f / (float)SDL_GetPerformanceFrequency());
+		deltaTime = (float)((currTime - prevTime)
+				/ (float)SDL_GetPerformanceFrequency());
 
 		while(SDL_PollEvent(&event)) {
-			if(event.type == SDL_QUIT) {
-				quit = true;
+			switch(event.type) {
+				case SDL_QUIT:
+					quit = true;
+					break;
+				case SDL_KEYDOWN:
+					switch(event.key.keysym.sym) {
+						case SDLK_ESCAPE:
+							quit = true;
+							break;
+						default:
+							break;
+					}
+					break;
+				default:
+					break;
 			}
 		}
 
@@ -120,7 +118,7 @@ int main(void) {
 
 		if(cam.rx < -M_PI_2)
 			cam.rx = -M_PI_2;
-		if(cam.rx > M_PI_2)
+		else if(cam.rx > M_PI_2)
 			cam.rx = M_PI_2;
 
 		if(keys[SDL_SCANCODE_W]) {
@@ -144,52 +142,77 @@ int main(void) {
 		if(keys[SDL_SCANCODE_Q])
 			cam.y -= MOVE_SPEED * deltaTime;
 
-		SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+		glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		glBegin(GL_POINTS);
 		for(size_t i = 0; i < LENGTH(points); i++) {
-			float3d dp = viewTransform(points[i]);
+			Float3d dp = viewTransform(points[i]);
 			if(dp.z > 0) {
-				SDL_RenderFillRect(renderer, &(SDL_Rect){
-						dp.x + SCREEN_WIDTH / 2 - POINT_SIZE / 2,
-						SCREEN_HEIGHT / 2 - dp.y - POINT_SIZE / 2,
-						POINT_SIZE,
-						POINT_SIZE});
+				glVertex2f(dp.x, dp.y * SCREEN_WIDTH / SCREEN_HEIGHT);
 			}
 		}
+		glEnd();
 
+		glBegin(GL_LINES);
 		for(size_t i = 0; i < LENGTH(lines); i++) {
-			float3d p1 = viewTransform(points[lines[i][0]]);
-			float3d p2 = viewTransform(points[lines[i][1]]);
-			if(p1.z > 0 || p2.z > 0) {
-				if(p1.z < 0 || p2.z < 0) {
-					if(p2.z < 0) {
-						float3d const swp = p1;
-						p1 = p2;
-						p2 = swp;
-					}
-					float const f = p1.z / (p2.z - p1.z);
-					p1 = (float3d){
-						(p1.x * f - p2.x * (f - 1.0f)) / (p2.z - p1.z),
-						(p1.y * f - p2.y * (f - 1.0f)) / (p2.z - p1.z),
-						0
-					};
-				}
-				SDL_RenderDrawLine(renderer,
-						p1.x + SCREEN_WIDTH / 2,
-						SCREEN_HEIGHT / 2 - p1.y,
-						p2.x + SCREEN_WIDTH / 2,
-						SCREEN_HEIGHT / 2 - p2.y);
+			Float3d p1 = viewTransform(points[lines[i][0]]);
+			Float3d p2 = viewTransform(points[lines[i][1]]);
+
+			if(zClipLine(&p1, &p2)) {
+				glVertex2f(p1.x, p1.y * SCREEN_WIDTH / SCREEN_HEIGHT);
+				glVertex2f(p2.x, p2.y * SCREEN_WIDTH / SCREEN_HEIGHT);
 			}
 		}
+		glEnd();
 
-		SDL_RenderPresent(renderer);
+		SDL_GL_SwapWindow(window);
 	}
 
-	SDL_DestroyRenderer(renderer);
+	SDL_GL_DeleteContext(glcontext);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return EXIT_SUCCESS;
 };
+
+Float3d viewTransform(Float3d p) {
+	float x = p.x - cam.x;
+	float y = p.y - cam.y;
+	float z = p.z - cam.z;
+
+	float const z2 = x * cosf(cam.ry) + z * sinf(cam.ry);
+	x = z * cosf(cam.ry) - x * sinf(cam.ry);
+	z = z2 * cosf(cam.rx) - y * sinf(cam.rx);
+	y = z2 * sinf(cam.rx) + y * cosf(cam.rx);
+
+	if(z == 0.0f)
+		z = 0.0000001f;
+	x *= ZSCALE / z;
+	y *= ZSCALE / z;
+
+	return (Float3d){ x, y, z };
+}
+
+bool zClipLine(Float3d *p1, Float3d *p2) {
+	float f;
+
+	if(p1->z > 0.0f && p2->z > 0.0f)
+		return true;
+	if(p1->z <= 0.0f && p2->z <= 0.0f)
+		return false;
+
+	if(p2->z <= 0.0f) {
+		Float3d *const swp = p1;
+		p1 = p2;
+		p2 = swp;
+	}
+
+	f = p1->z / (p2->z - p1->z);
+	*p1 = (Float3d){
+			(p1->x * f - p2->x * (f - 1.0f)) / (p2->z - p1->z),
+			(p1->y * f - p2->y * (f - 1.0f)) / (p2->z - p1->z),
+			0.0f
+	};
+	return true;
+}
---
+++ 3D/types.h
@@ -0,0 +1,8 @@
+typedef struct {
+	float x, y, z;
+} Float3d;
+
+typedef struct {
+	float x, y, z;
+	float rx, ry;
+} Camera;
