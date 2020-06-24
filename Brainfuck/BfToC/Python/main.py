5f0b7a5340e25288a73fba7fcee1799f8915a216
Johan-Mi <johanmilanov@outlook.com>
2020-06-24 10:18:56 +0200

    -

--- Brainfuck/BfToC/Python/main.py
+++ Brainfuck/BfToC/Python/main.py
@@ -6,76 +6,80 @@ from enum import Enum
 class Token(Enum):
     Input, Output, Inc, Move, LoopBegin, LoopEnd, Set = range(7)
 
-with open('../program.bf') as f:
-    prog = re.sub(r'[^-\+,\.<>\[\]]', '', f.read())
-prog = prog.replace('[-]', '0')
-output = []
+def main():
+    with open('../program.bf') as prog_file:
+        prog = re.sub(r'[^-\+,\.<>\[\]]', '', prog_file.read())
+    prog = prog.replace('[-]', '0')
+    output = []
 
-i = 0
-while i < len(prog):
-    if prog[i] == '0':
-        output.append((Token.Set, 0))
-    elif prog[i] == ',':
-        output.append((Token.Input, None))
-    elif prog[i] == '.':
-        output.append((Token.Output, None))
-    elif prog[i] == '[':
-        output.append((Token.LoopBegin, None))
-    elif prog[i] == ']':
-        output.append((Token.LoopEnd, None))
-    elif prog[i] in '+-':
-        changeAmount = 0
-        while prog[i] in '+-':
-            changeAmount += 1 if prog[i] == '+' else -1
-            i += 1
-        i -= 1
-        output.append((Token.Inc, changeAmount))
-    elif prog[i] in '<>':
-        changeAmount = 0
-        while prog[i] in '<>':
-            changeAmount += 1 if prog[i] == '>' else -1
-            i += 1
-        i -= 1
-        output.append((Token.Move, changeAmount))
-    i += 1
+    i = 0
+    while i < len(prog):
+        if prog[i] == '0':
+            output.append((Token.Set, 0))
+        elif prog[i] == ',':
+            output.append((Token.Input, None))
+        elif prog[i] == '.':
+            output.append((Token.Output, None))
+        elif prog[i] == '[':
+            output.append((Token.LoopBegin, None))
+        elif prog[i] == ']':
+            output.append((Token.LoopEnd, None))
+        elif prog[i] in '+-':
+            change_amount = 0
+            while prog[i] in '+-':
+                change_amount += 1 if prog[i] == '+' else -1
+                i += 1
+            i -= 1
+            output.append((Token.Inc, change_amount))
+        elif prog[i] in '<>':
+            change_amount = 0
+            while prog[i] in '<>':
+                change_amount += 1 if prog[i] == '>' else -1
+                i += 1
+            i -= 1
+            output.append((Token.Move, change_amount))
+        i += 1
 
-with open('../output.c', 'w') as out:
-    out.write('#include <stdio.h>\n#include <stdlib.h>\n\nint main(void) {\n\tunsigned short ptr = 0;\n\tunsigned char memory[65536] = {0};\n\n')
-    indentation = 1
-    for token, value in output:
-        if token == Token.Set:
-            line = 'memory[ptr] = %d;' % value
-        elif token == Token.Input:
-            line = 'memory[ptr] = getchar();'
-        elif token == Token.Output:
-            line = 'putchar(memory[ptr]);'
-        elif token == Token.Inc:
-            if value == 1:
-                line = 'memory[ptr]++;'
-            elif value == -1:
-                line = 'memory[ptr]--;'
-            elif value > 0:
-                line = 'memory[ptr] += %d;' % value
-            else:
-                line = 'memory[ptr] -= %d;' % -value
-        elif token == Token.Move:
-            if value == 1:
-                line = 'ptr++;'
-            elif value == -1:
-                line = 'ptr--;'
-            elif value > 0:
-                line = 'ptr += %d;' % value
-            else:
-                line = 'ptr -= %d;' % -value
-        elif token == Token.LoopBegin:
-            line = 'while(memory[ptr]) {'
-        elif token == Token.LoopEnd:
-            line = '}'
-            indentation -= 1
+    with open('../output.c', 'w') as out:
+        out.write('#include <stdio.h>\n#include <stdlib.h>\n\nint main(void) {\n\tunsigned short ptr = 0;\n\tunsigned char memory[65536] = {0};\n\n')
+        indentation = 1
+        for token, value in output:
+            if token == Token.Set:
+                line = 'memory[ptr] = %d;' % value
+            elif token == Token.Input:
+                line = 'memory[ptr] = getchar();'
+            elif token == Token.Output:
+                line = 'putchar(memory[ptr]);'
+            elif token == Token.Inc:
+                if value == 1:
+                    line = 'memory[ptr]++;'
+                elif value == -1:
+                    line = 'memory[ptr]--;'
+                elif value > 0:
+                    line = 'memory[ptr] += %d;' % value
+                else:
+                    line = 'memory[ptr] -= %d;' % -value
+            elif token == Token.Move:
+                if value == 1:
+                    line = 'ptr++;'
+                elif value == -1:
+                    line = 'ptr--;'
+                elif value > 0:
+                    line = 'ptr += %d;' % value
+                else:
+                    line = 'ptr -= %d;' % -value
+            elif token == Token.LoopBegin:
+                line = 'while(memory[ptr]) {'
+            elif token == Token.LoopEnd:
+                line = '}'
+                indentation -= 1
 
-        out.write('\t' * indentation + line + '\n')
+            out.write('\t' * indentation + line + '\n')
 
-        if token == Token.LoopBegin:
-            indentation += 1
+            if token == Token.LoopBegin:
+                indentation += 1
 
-    out.write('\n\treturn EXIT_SUCCESS;\n}')
+        out.write('\n\treturn EXIT_SUCCESS;\n}')
+
+if __name__ == "__main__":
+    main()
--- Calculator/main.py
+++ Calculator/main.py
@@ -1,71 +1,79 @@
 #!/usr/bin/env python3
 
+
 def calc(expr):
-	try:
-		return float(expr)
-	except:
-		if expr in variables.keys():
-			return variables[expr]
+    try:
+        return float(expr)
+    except ValueError:
+        if expr in variables.keys():
+            return variables[expr]
 
-		i = expr.find('(')
-		if ('(' in expr) ^ (')' in expr):
-			raise Exception
-		if i != -1:
-			j = i
-			brackets = 1
-			while brackets:
-				j += 1
-				if expr[j] == '(':
-					brackets += 1
-				elif expr[j] == ')':
-					brackets -= 1
-			return calc(expr[:i] + str(calc(expr[i + 1:j])) + expr[j + 1:])
+        i = expr.find('(')
+        if ('(' in expr) ^ (')' in expr):
+            raise Exception
+        if i != -1:
+            j = i
+            brackets = 1
+            while brackets:
+                j += 1
+                if expr[j] == '(':
+                    brackets += 1
+                elif expr[j] == ')':
+                    brackets -= 1
+            return calc(expr[:i] + str(calc(expr[i + 1:j])) + expr[j + 1:])
 
-		i = expr.rfind('+')
-		j = expr.rfind('-')
-		if i != -1 and (j == -1 or j < i):
-			return calc(expr[:i]) + calc(expr[i + 1:])
-		if j != -1 and (i == -1 or i < j):
-			return calc(expr[:j]) - calc(expr[j + 1:])
+        i = expr.rfind('+')
+        j = expr.rfind('-')
+        if i != -1 and (j == -1 or j < i):
+            return calc(expr[:i]) + calc(expr[i + 1:])
+        if j != -1 and (i == -1 or i < j):
+            return calc(expr[:j]) - calc(expr[j + 1:])
 
-		i = expr.rfind('*')
-		j = expr.rfind('/')
-		if i != -1 and (j == -1 or j < i):
-			return calc(expr[:i]) * calc(expr[i + 1:])
-		if j != -1 and (i == -1 or i < j):
-			try:
-				return calc(expr[:j]) / calc(expr[j + 1:])
-			except ZeroDivisionError as e:
-				raise e
+        i = expr.rfind('*')
+        j = expr.rfind('/')
+        if i != -1 and (j == -1 or j < i):
+            return calc(expr[:i]) * calc(expr[i + 1:])
+        if j != -1 and (i == -1 or i < j):
+            try:
+                return calc(expr[:j]) / calc(expr[j + 1:])
+            except ZeroDivisionError as err:
+                raise err
+
 
 variables = {}
 
-while True:
-	inExpr = input('> ')
 
-	if inExpr.lower() in ['quit', 'q', 'exit']:
-		break
+def main():
+    while True:
+        in_expr = input('> ')
 
-	inExpr = inExpr.replace(' ', '')
+        if in_expr.lower() in ['quit', 'q', 'exit']:
+            break
 
-	try:
-		i = inExpr.find('=')
-		if i != -1:
-			start = i + 1
-			var = inExpr[:i]
-		else:
-			start = 0
-			var = None
+        in_expr = in_expr.replace(' ', '')
 
-		result = calc(inExpr[start:])
-		if result == int(result):
-			result = int(result)
+        try:
+            i = in_expr.find('=')
+            if i != -1:
+                start = i + 1
+                var = in_expr[:i]
+            else:
+                start = 0
+                var = None
 
-		if var is not None:
-			variables[var] = result
+            result = calc(in_expr[start:])
+            if result == int(result):
+                result = int(result)
 
-		print(result)
-	except ZeroDivisionError:
-		print('Tried to divide by zero!')
-	except:
-		print('Syntax error!')
+            if var is not None:
+                variables[var] = result
+
+            print(result)
+        except ZeroDivisionError:
+            print('Tried to divide by zero!')
+        except:
+            print('Syntax error!')
+
+
+if __name__ == "__main__":
+    main()
